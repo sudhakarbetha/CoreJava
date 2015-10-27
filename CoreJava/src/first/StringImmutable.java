da185702ec2b708d5fdd5ae5e1c1c71978a5f2c0
Sudhakar <sudhakar.betha@gmail.com>
2015-10-28 00:12:04 +0530

    string

--- CoreJava/src/first/StringImmutable.java
+++ CoreJava/src/first/StringImmutable.java
@@ -3,8 +3,12 @@ package first;
 public class StringImmutable {
 	 public static void main(String[] args)
 	    {
+		   //Creates object on String Literal Pool
 	        String one = "someString";
+	        
+	        //Creates a new Object
 	        String two = new String("someString");
+	        
 	        
 	        System.out.println(one.equals(two));
 	        System.out.println(one == two);
